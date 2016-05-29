dec059e6f958ae32902159ac28c6bd537548237d
rgrannell1 <r.grannell2@gmail.com>
2016-05-29 13:42:09 +0100

    corrected total count value

--- src/python/solve-polynomials.py
+++ src/python/solve-polynomials.py
@@ -14,9 +14,12 @@ Options:
 
 import os
 import sys
+import json
 import cmath
 import numpy
 import itertools
+from functools import reduce
+from operator  import mul
 import time
 
 from docopt   import docopt
@@ -51,11 +54,9 @@ def counter ( ):
 		ith = ith + 1
 		yield ith
 
-def prompt_start (order, num_range):
+def prompt_start (order, num_range, total_count):
 
-	count = ((num_range * 2) + 1) ** order
-
-	message = 'attempting to solve ' + '{:,}'.format(count) + ' polynomials. Do you want to start?'
+	message = 'attempting to solve ' + '{:,}'.format(total_count) + ' polynomials. Do you want to start?'
 	answer  = yesno(message, default = 'no')
 
 	if not answer:
@@ -72,56 +73,82 @@ def print_progress (iter, total_count, start):
 	if iter % constants["print_frequency"] == 0:
 
 		end        = time.time( )
-		elapsed    = round(end - start)
+		elapsed    = end - start
 		per_second = round(iter / elapsed)
 
 		estimated_per_hour = per_second * 60 * 60
 		second_remaining   = round((total_count - iter) / per_second)
 
+		messages           = [
+			'average solved per second: ' + str(per_second) + '\n',
+			'total solved:              ' + str(iter) + '\n',
+			'seconds remaining:         ' + str(second_remaining) + '\n',
+			'estimated per hour:        ' + str(estimated_per_hour) + '\n'
+		]
+
 		if iter >= 2 * constants["print_frequency"]:
-			eraseLines(4)
+			eraseLines(len(messages))
 
-		sys.stderr.write('average solved per second: ' + str(per_second) + '\n')
-		sys.stderr.write('total solved: '              + str(iter) + '\n')
-		sys.stderr.write('seconds remaining: '        + str(second_remaining) + '\n')
-		sys.stderr.write('estimated per hour: '        + str(estimated_per_hour) + '\n')
+		for message in messages:
+			sys.stderr.write(message)
 
+def write_solutions(solution, solution_buffer, force = False):
 
-def write_roots(roots, root_buffer):
-
-	if len(root_buffer) == constants["flush_threshold"]:
+	if len(solution_buffer) == constants["flush_threshold"] or force:
 
 		with open(constants['paths']['output'], "a") as fpath:
+			fpath.write(json.dumps(solution) + '\n')
 
-			for root in root_buffer:
-				fpath.write(','.join([str(num) for num in root]) + '\n')
+		del solution_buffer[:]
 
-		del root_buffer[:]
+	solution_buffer.append(solution)
 
-	root_buffer += roots
+def solve_polynomial (point):
 
-def to_coefficients (point, roots):
-	return [[root.real, root.imag] for root in roots]
+	return {
+		'coefficients': point,
+		'roots':        [ [root.real, root.imag] for root in numpy.roots(point) ]
+	}
 
+def repeat_val (num, val):
+	return [val for _ in range(num - 1)]
 
+def product (nums):
+	return reduce(mul, nums, 1)
+
+def sequence (lower, upper):
+
+	seq = [ ]
+	current = lower
+
+	while current <= upper:
+		seq.append(current)
+		current += 1
+
+	return seq
 
 def solve_polynomials (order, num_range):
 
-	prompt_start(order, num_range)
 
-	dimensions  = (range(-(num_range - 1), num_range) for _ in range(order))
+	dimensions  = repeat_val(order, sequence(-num_range, num_range))
 	space       = itertools.product(*dimensions)
+
+	total_count = product(len(val) for val in dimensions)
+
+	prompt_start(order, num_range, total_count)
 
 	start       = time.time( )
 	root_count  = counter( )
-	root_buffer = [ ]
+	solution_buffer = [ ]
 
-	total_count = ((2 * num_range) + 1) ** order
+	for point in space:
 
-	for roots in (to_coefficients(point, numpy.roots(point)) for point in space):
+		solution = solve_polynomial(point)
 
 		print_progress(next(root_count), total_count, start)
-		write_roots(roots, root_buffer)
+		write_solutions(solution, solution_buffer)
+
+	write_solutions(solution, solution_buffer, force = True)
 
 
 
